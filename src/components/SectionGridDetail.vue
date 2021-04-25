e88d28aec372bfe82808ed0caf6d360037bee272
Jo <chiron1217@naver.com>
2021-04-25 21:36:52 +0900

    init

---
+++ src/components/SectionGridDetail.vue
@@ -0,0 +1,19 @@
+<template >
+    <div id="vue-section-grid-detail">
+        
+    </div>
+</template>
+<script>
+export default {
+    name:"vue-section-grid-detail",
+    props:{},
+    data() {
+        return {
+            
+        }
+    },
+}
+</script>
+<style >
+    
+</style>
